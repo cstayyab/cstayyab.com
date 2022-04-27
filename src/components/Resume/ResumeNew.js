0cfb915fe29b07cee78e2c85df4679ebda3dfa6f
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2022-04-27 08:11:27 +0500

    🐛 Display 2nd page of Resume too

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -30,13 +30,17 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+
+          <Document file={resumeLink} className="d-flex justify-content-center mt-2">
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
@@ -48,7 +52,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
       </Container>
