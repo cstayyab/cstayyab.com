1e1d9fdd061397eff48756c40c8c74c08aa67f31
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2022-04-27 08:12:57 +0500

    🐛 Updating Links in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href="https://dev.to/cstayyab/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/cstayyab/cstayyab.com"
                 target="_blank"
                 className="fork-btn-inner"
               >
